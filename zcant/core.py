Refactor wx GUI thread hook out of non-GUI `core` module; fix slope calculation in compressed view

zcant/core.py
@@ -19,8 +19,6 @@ from guano import GuanoFile
 
 import numpy as np
 
-import wx
-
 import logging
 log = logging.getLogger(__name__)
 
@@ -152,7 +150,7 @@ def _slopes(x, y, max_slope=5000):
         y_octaves = y
     else:
         # calculation for difference wil be same in Hz or kHz, so no need to convert
-        y_octaves = np.log2(y)
+        y_octaves = np.log2(y)  # FIXME: FloatingPointError: divide by zero encountered in log2
         y_octaves[np.isnan(y_octaves)] = 0.0
     slopes = np.diff(y_octaves) / np.diff(x)
     slopes = np.append(slopes, slopes[-1])  # FIXME: hack for final dot (instead merge slope(signal[1:]) and slope(signal[:-1])
@@ -192,7 +190,12 @@ class MainThread(Thread):
         else:
             raise Exception('Unknown file type: %s', path)
 
+    def on_complete(self, result):
+        """Called upon completion of run(), may be overridden by subclasses"""
+        self.parent_cb(result)
+
     def run(self):
+        """Thread main"""
         result = None
         try:
             times, freqs, amplitudes, metadata = self.extract(self.path)
@@ -204,7 +207,8 @@ class MainThread(Thread):
             result = ZeroCross(times, freqs, amplitudes, metadata)
         except Exception:
             log.exception('Barfed loading file: %s', self.path)
-        wx.CallAfter(self.parent_cb, result)
+
+        self.on_complete(result)
 
 
 class AnabatFileWriteThread(Thread):

zcant/gui.py
@@ -769,7 +769,7 @@ class ZcantMainFrame(wx.Frame, wx.FileDropTarget):
                       interpolation=self.wav_interpolation)
 
         wx.BeginBusyCursor()
-        MainThread(self.after_load, path, **kwargs)
+        WxMainThread(self.after_load, path, **kwargs)
 
     def after_load(self, result):
         # callback when we return from asynchronous MainThread
@@ -1145,7 +1145,9 @@ class ZeroCrossPlotPanel(PlotPanel):
             x1, y1 = eclick.xdata, eclick.ydata
             x2, y2 = erelease.xdata, erelease.ydata
             log.debug(' Select  (%.3f,%.1f) -> (%.3f,%.1f)  button: %d' % (x1, y1, x2, y2, eclick.button))
-            slope = (np.log2(y2) - np.log2(y1)) / (x2 - x1)  # FIXME: we don't support compressed mode here!
+            if self.config['compressed']:
+                x1, x2 = self.zc.times[int(round(x1))], self.zc.times[int(round(x2))]
+            slope = (np.log2(y2) - np.log2(y1)) / (x2 - x1)
             log.debug('         slope: %.1f oct/sec  (%.1f kHz / %.3f sec)' % (slope, y2 - y1, x2 - x1))
 
         self.selector = RectangleSelector(dot_plot, onselect, drawtype='box')
@@ -1212,6 +1214,13 @@ class ZeroCrossPlotPanel(PlotPanel):
     #     if event.inaxes:
     #         x, y = event.xdata, event.ydata
     #         print '%.1fkHz, %.1f' % (y, x)
+
+
+class WxMainThread(MainThread):
+    """Main thread handler which hooks back into wx GUI thread upon completion"""
+
+    def on_complete(self, result):
+        wx.CallAfter(self.parent_cb, result)
 
 
 def launch_external(fname):